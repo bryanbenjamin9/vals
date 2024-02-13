d9b85e6db949a95ecb86930f9d12cdbc65098f33
bryanbenjamin9 <60759618+bryanbenjamin9@users.noreply.github.com>
2024-02-13 16:17:51 +0000

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -13,14 +13,14 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'user@example.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'secret';                               //SMTP password (your gmail password or app password)
+    $mail->Username   = 'owusukonadubryan@gmail.com';                     //SMTP username (your gmail account)
+    $mail->Password   = 'Brianbenjaminok,123';                               //SMTP password (your gmail password or app password)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('owusukonaduryan@gmail.com', 'Brian');         //Set the sender of the message (your email address)
+    $mail->addAddress('wthumbil@gmail.com', 'Babe');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
